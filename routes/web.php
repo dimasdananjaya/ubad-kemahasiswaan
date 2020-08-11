9f35b91337aca024a44a4d2529ca2d5075837c22
dimasdananjaya <gededimas@gmail.com>
2020-08-11 22:34:51 +0800

    composing tools for sweetalert, auth, laravel collective

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'RouteController@index')->name('home');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
